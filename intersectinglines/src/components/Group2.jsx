da7c45c1787ef81812517941f8b4c77939563328
jdmartinezrs <157431644+jdmartinezrs@users.noreply.github.com.>
2024-11-03 21:16:56 -0500

    feat: :sparkles: The third lines group is already done

--- intersectinglines/src/components/Group2.jsx
+++ intersectinglines/src/components/Group2.jsx
@@ -6,19 +6,15 @@ const Group2 = () => {
             <div className="container__2">
                 <div className="horizontal_bar2">
                     <div className="h-bar2"></div>
+                    <div className="h-bar2__superpuesta"></div>
                     <div className="h-bar2"></div>
-                    <div className="h-bar2"></div>
-                    <div className="h-bar2"></div>
+                    <div className="h-bar2__superpuesta"></div>
                 </div>
                 
                 <div className="vertical_bar2">
-                    <div className="v-bar2__primera"></div>
-                    <div className="v-bar2__primera__1"></div>
-                    <div className="v-bar2__primera__2"></div>
-                    <div className="v-bar2__primera__3"></div>
-                    <div className="v-bar2__primera__4"></div>
+                    <div className="v-bar2__atras"></div>
                     <div className="v-bar2"></div>
-                    <div className="v-bar2"></div>
+                    <div className="v-bar2__atras"></div>
                     <div className="v-bar2"></div>
                 </div>
             </div>
